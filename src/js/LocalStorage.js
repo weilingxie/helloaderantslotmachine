d60661af2a6ecbb204d3b7f60cc00a3156e2fe94
William Xie <william.xie@aderant.com>
2019-03-25 16:30:48 +1300

    Modify deposit modal title

--- src/js/LocalStorage.js
+++
@@ -1,8 +0,0 @@
-
-const setLocalStorage = (status) => {
-    localStorage.setItem("status", status);
-}
-
-const getLocalStorage = () => localStorage.getItem("status");
-
-export default { setLocalStorage, getLocalStorage };
